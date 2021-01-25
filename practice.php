607b3d351020f4db5f23adff0076d7fee59863ae
Alex_Kelly <afkelly14@Gmail.com>
2021-01-25 16:09:23 -0500

    php practice

---
+++ practice.php
@@ -0,0 +1,45 @@
+//Insert the missing part of the code below to output "Hello World".
+<?php
+echo "Hello world";
+?>
+
+//Write the correct opening tag and close tag for PHP scripts.
+<?php 
+echo "This is PHP";
+?>
+
+//Statements in PHP have to end with a special character
+<?php
+echo "Hello World";
+?>
+
+//Create a variable named txt and assign the value "Hello".
+<?php
+$txt = "Hello";
+?>
+
+//Create one variable named x, and one variable named y, 
+//then use the echo statement to output the sum of x and y.
+<?php
+$x = 5;
+$y = 7;
+echo $x + $y;
+?>
+
+
+//Get the length of the string "Hello World!".
+<?php
+echo strlen("Hello World!");
+?>
+
+
+//Reverse the string "Hello World!".
+<?php
+echo strrev("Hello World!");
+?>
+
+//Replace the word "World" with the word "Dolly".
+<?php
+$oldtxt = "Hello World";
+$newtxt = str_replace("World", "Dolly", $oldtxt);
+?>
